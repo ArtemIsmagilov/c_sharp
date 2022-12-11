7f7496633f054ec186dc79d049742f83195c7f51
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2022-12-11 22:55:49 +0400

    add hw6

---
+++ homework6/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+void more_zero(string text)
+{
+    text += " ";
+
+    int len = text.Length;
+    string dig = "";
+    int count = 0;
+    for (int i = 0; i < len; i += 1)
+    {
+        if (text[i] == ' ' && dig.Length > 0)
+        {
+            float float_dig = float.Parse(dig);
+            dig = "";
+            if (float_dig > 0)
+            {
+                count += 1;
+            }
+        }
+        else if (text[i] != ' ')
+        {
+            dig += Convert.ToString(text[i]);
+        }
+    }
+    Console.WriteLine($"-> Кол-во чисел > 0: {count}");
+
+}
+ Console.Write("Введите вещественные числа через пробел: ");
+more_zero(Console.ReadLine());
---
+++ homework6/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+void x_y(float b1, float k1, float b2, float k2)
+{
+    Console.WriteLine($"b1: {k1}\nk1: {b1}\nb2: {k2}\nk2: {b2}");
+    float x = (b2-b1)/(k1-k2);
+    float y = k2*x + b2;
+    Console.WriteLine($"x: {x}\ny: {y}\nТочка: ({x}, {y})");
+}
+
+float inp() {return float.Parse(Console.ReadLine());}
+Console.WriteLine("b1, k1, b2, k2");
+x_y(inp(), inp(), inp(), inp());
